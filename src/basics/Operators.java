9e730d3ba45d5ef910411360fa61444a8842fbef
Czarny <mariusz.czarny.external@atos.net>
2018-04-11 14:15:15 +0200

    Operators

---
+++ src/basics/Operators.java
@@ -0,0 +1,36 @@
+package basics;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		String testVariable = "some text";
+		System.out.println(checkValue(testVariable));
+		int result = (testVariable.equals("some text")) ? 0 : 1;
+		System.out.println(result);
+
+		int x = 1;
+
+		// 'Geeks in &&' is NOT printed because x is 0
+		if (x == 0 && printInfo()) {
+			System.out.println("Operator logiczny zadzia³a³");
+		}
+
+		// 'Geeks in &' is printed
+		if (x == 0 & printInfo()) {
+			System.out.println("Operator bitowy zadzia³a³");
+		}
+	}
+
+	private static boolean printInfo() {
+		System.out.println("Metoda siê wykona³a");
+		return true;
+	}
+
+	private static int checkValue(String testVariable) {
+		if (testVariable.equals("some text")) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+}
