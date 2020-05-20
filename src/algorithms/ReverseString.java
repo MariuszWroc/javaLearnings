9185bb8f62de73eafa73ecb3f69476b5f97df376
Mariusz Czarny <mariusz.czarny88@gmail.com>
2020-05-20 10:01:57 +0200

    Renaming

--- src/algorithms/ReverseString.java
+++ src/algorithms/ReverseString.java
@@ -2,7 +2,7 @@ package algorithms;
 
 import java.util.Arrays;
 
-public class RiverseString {
+public class ReverseString {
 
 	public static void main(String[] args) {
 		String input = "GeeksForGeeks"; 
