9a9c5c34c4880a9276e41bb9360bbbde85c633e2
Mariusz Czarny <mariusz.czarny88@gmail.com>
2020-05-26 07:17:14 +0200

    General refactor

---
+++ src/main/java/advanced/tests/MyCalculator.java
@@ -0,0 +1,20 @@
+package advanced.tests;
+
+public class MyCalculator {
+
+	public int add(Integer numberOne, Integer numberTwo) {
+		return numberOne + numberTwo;
+	}
+
+	private int subtract(Integer numberOne, Integer numberTwo) {
+		return numberOne - numberTwo;
+	}
+
+	protected int multiply(Integer numberOne, Integer numberTwo) {
+		return numberOne * numberTwo;
+	}
+
+	public double divide(Integer numberOne, Integer numberTwo) {
+		return (double) numberOne / numberTwo;
+	}
+}
