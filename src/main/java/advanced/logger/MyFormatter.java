823e5c76ea556d6b50fe4be08d0de851bd4ca89e
Mariusz Czarny <mariusz.czarny88@gmail.com>
2020-05-27 20:26:03 +0200

    Logger

---
+++ src/main/java/advanced/logger/MyFormatter.java
@@ -0,0 +1,15 @@
+package advanced.logger;
+
+import java.time.LocalDate;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class MyFormatter extends Formatter {
+
+	@Override
+	public String format(LogRecord record) {
+		return record.getSourceClassName() + ", " + record.getSourceMethodName() + ", " 
+				+ LocalDate.now() + "\n";
+	}
+
+}
