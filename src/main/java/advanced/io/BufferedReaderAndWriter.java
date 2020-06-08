563ec72398e4a1f9034e4edddeec93793bbf3b63
Mariusz Czarny <mariusz.czarny88@gmail.com>
2020-06-08 10:59:09 +0200

    Input output streams

--- src/main/java/advanced/io/BufferedReaderAndWriter.java
+++ src/main/java/advanced/io/BufferedReaderAndWriter.java
@@ -2,7 +2,6 @@ package advanced.io;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
